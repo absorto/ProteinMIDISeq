1c967b295cea17c4818e7477b1a8f02fdf54d811
Rodrigo García <absorto@sdf.org>
2014-07-03 00:02:35 -0500

    loop over an array, play it via timidity, adjustable velocity

---
+++ loop.py
@@ -0,0 +1,52 @@
+import pprint
+from time import sleep
+import mido
+import random
+
+output = mido.open_output( u'TiMidity port 0')
+#woodblock
+#output.send(mido.Message('program_change', program=115))
+
+output.send(mido.Message('program_change', program=45))
+
+
+loop = [
+    [1, 0, 1, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+# C3 = 48
+notas = range(48,80,2)
+
+bpm = 111
+delay = 60.0 / bpm
+
+try:
+    n = 0
+    while True:
+        sleep(delay)
+        
+        for i in range(0,len(loop[n])):
+            if loop[n][i]:
+                output.send(mido.Message('note_on', note=notas[i], velocity=64))
+            else:
+                output.send(mido.Message('note_off', note=notas[i], velocity=64))
+
+        print loop[n]
+
+        # increment or restart loop
+        n+=1
+        if n==len(loop):
+            n = 0
+            # output.send(mido.Message('program_change', program=random.randrange(70,80)))
+        
+
+except KeyboardInterrupt:
+    for i in notas:
+        output.send(mido.Message('note_on', note=notas[i], velocity=64))
