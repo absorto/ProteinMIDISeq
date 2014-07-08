7d5c3a727f84f6d26b5831c24d38b524b089a576
Rodrigo Garcia <absorto@sdf.org>
2014-07-08 14:59:21 -0500

    t is for time, note_off everything on keyboardinterrupt

--- loop.py
+++ loop.py
@@ -11,14 +11,18 @@ output.send(mido.Message('program_change', program=80))
 
 
 loop = [
-    [1, 0, 1, 0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 1, 0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 1, 0],
+    [1, 0, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 0, 1],
+    [0, 1, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0],
     ]
 
 # C3 = 48
@@ -28,27 +32,26 @@ bpm = 111
 delay = 60.0 / bpm
 
 try:
-    n = 0
+    t = 0
     while True:
         sleep(delay)
         
-        for i in range(0,len(loop[n])):
-            if loop[n][i]:
-                if not loop[n-1][i]:
+        for i in range(0,len(loop[t])):
+            if loop[t][i]:
+                if not loop[t-1][i]:
                     output.send(mido.Message('note_on', note=notas[i], velocity=64))
             else:
-                if loop[n-1][i]:
+                if loop[t-1][i]:
                     output.send(mido.Message('note_off', note=notas[i], velocity=64))
 
-        print loop[n]
+        print loop[t]
 
-        # increment or restart loop
-        n+=1
-        if n==len(loop):
-            n = 0
-            # output.send(mido.Message('program_change', program=random.randrange(70,80)))
+        # as time goes by
+        t+=1
+        if t == len(loop):
+            t = 0
         
 
 except KeyboardInterrupt:
-    for i in notas:
-        output.send(mido.Message('note_on', note=notas[i], velocity=64))
+    for i in range(0,len(loop[0])):
+        output.send(mido.Message('note_off', note=notas[i], velocity=64))
