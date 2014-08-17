f8b0f7525ad4124312fcd6de5f327ef90101e17e
Rodrigo García <absorto@sdf.org>
2014-08-16 19:59:09 -0500

    python modularization, encapsulate playhead tick

---
+++ loop/__init__.py
@@ -0,0 +1,120 @@
+# coding: utf8
+
+
+import mido
+import pyglet
+
+
+# un objeto loop seguramente tendrá dimensiones visuales y lógicas, y siempre va pegado a un sink midi
+# al llamarse tendrian que darle sus dimensiones, y el tendria que redimensionarse apropiadamente
+# al loop habría que darle: (width, height), (x,y), roll, midi_sink
+class Loop:
+
+    playhead_x = 0
+    tt         = 0
+    start      = True
+
+    def __init__(self, loop, scale, width=800, height=600, midi_port=u'TiMidity port 0', midi_chan=1):
+        self.loop       = loop
+        self.beat_width = 30
+        self.width      = width
+        self.height     = height
+        self.scale      = scale
+        self.beat_width = float(width)/len(self.loop)
+        self.beat_height = float(height)/len(self.loop[0])
+
+        print self.beat_width, self.beat_height
+
+        # initialize midi output
+        self.midi_output = mido.open_output( midi_port )
+        self.midi_output.send(mido.Message('program_change', program=midi_chan))
+
+        # load sprites
+        self.on = pyglet.image.load('resources/on.png') 
+        self.on_sprite = pyglet.sprite.Sprite( self.on )
+
+        self.off = pyglet.image.load('resources/off.png') 
+        self.off_sprite = pyglet.sprite.Sprite( self.off )
+
+
+
+    def render_playhead(self):
+        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                             ('v2f', (self.playhead_x, 0, self.playhead_x, self.height)))
+
+
+    def render_pianoroll(self):
+        for x in range(0,len(self.loop)):
+            for y in range(0,len(self.loop[0])):
+                if self.loop[x][y]:
+                    self.on.blit(x*self.beat_width, y*self.beat_height)
+                else:
+                    self.off.blit(x*self.beat_width, y*self.beat_height)
+
+
+    def midi_messages(self):
+
+        for i in range(0,len(self.loop[self.tt])):
+            if self.loop[self.tt][i]:
+                if not self.loop[self.tt-1][i]:
+                    self.midi_output.send(mido.Message('note_on', note=self.scale[i], velocity=64))
+            else:
+                if self.loop[self.tt-1][i]:
+                    self.midi_output.send(mido.Message('note_off', note=self.scale[i]))
+
+
+    def update_playhead_display(self, dt):
+
+        if self.start:
+            self.playhead_x = 1
+            self.start = False
+        else:
+            self.playhead_x += dt * 1.33 * self.beat_width
+
+        self.render_pianoroll()
+        self.render_playhead()
+
+    def play_at_head(self, dt):
+        if self.tt == len(self.loop)-1:
+            self.tt = 0
+            self.start = True
+        else:
+            self.tt+=1
+        self.midi_messages()
+
+
+
+
+
+class NoteConverter:
+    def __init__(self, base=5):
+        self.base = base
+        names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        self.notes = {}
+        for note in range(128):
+            name = names[note % 12]
+            octave = int(note / 12) - base
+            key = '%s%i' % (name, octave)
+            self.notes[key] = note
+            if octave == 0:
+                self.notes[name] = note
+
+    def __call__(self, note):
+        return self.notes.get(str(note).upper(), note)
+
+
+def loop_from_dna(sequence):
+    loop = []
+    # amino list from sequence
+#    amino_list = list(  Seq(sequence, IUPAC.unambiguous_dna).translate(to_stop=True).tostring() )
+    amino_list = list(  Seq(sequence, IUPAC.unambiguous_dna).translate().tostring() )
+    
+    standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
+
+    for amino in amino_list:
+        column = [0 for n in range(0,21)]
+        if amino != '*':
+            column[standard_table.back_table.keys().index(amino)] = 1
+
+        loop.append(column)
+    return loop
--- loop_pyglet.py
+++ loop_pyglet.py
@@ -1,102 +1,13 @@
 # coding: utf8
 import curses
 from time import sleep
-import mido
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 from Bio.Data import CodonTable
 
 import pyglet
 
-window = pyglet.window.Window(height=900)
-
-
-class NoteConverter:
-    def __init__(self, base=5):
-        self.base = base
-        names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-        self.notes = {}
-        for note in range(128):
-            name = names[note % 12]
-            octave = int(note / 12) - base
-            key = '%s%i' % (name, octave)
-            self.notes[key] = note
-            if octave == 0:
-                self.notes[name] = note
-
-    def __call__(self, note):
-        return self.notes.get(str(note).upper(), note)
-
-
-def loop_from_dna(sequence):
-    loop = []
-    # amino list from sequence
-#    amino_list = list(  Seq(sequence, IUPAC.unambiguous_dna).translate(to_stop=True).tostring() )
-    amino_list = list(  Seq(sequence, IUPAC.unambiguous_dna).translate().tostring() )
-    
-    standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
-
-    for amino in amino_list:
-        column = [0 for n in range(0,21)]
-        if amino != '*':
-            column[standard_table.back_table.keys().index(amino)] = 1
-
-        loop.append(column)
-    return loop
-
-
-# un objeto loop seguramente tendrá dimensiones visuales y lógicas, y siempre va pegado a un sink midi
-# al llamarse tendrian que darle sus dimensiones, y el tendria que redimensionarse apropiadamente
-# al loop habría que darle: (width, height), (x,y), roll, midi_sink
-class loop:
-
-    playhead_x = 0
-    tt         = 0
-    start      = True
-
-    def __init__(self, midi_output, loop):
-        self.loop = loop
-
-        self.output = midi_output
-        self.output.send(mido.Message('program_change', program=1))
-
-        self.on = pyglet.image.load('resources/on.png') 
-        self.on_sprite = pyglet.sprite.Sprite( self.on )
-
-        self.off = pyglet.image.load('resources/off.png') 
-        self.off_sprite = pyglet.sprite.Sprite( self.off )
-
-        self.width = len(loop)*30
-
-
-    def render_playhead(self):
-        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                             ('v2f', (self.playhead_x, 0, self.playhead_x, 1000)))
-
-
-    def render_pianoroll(self):
-        for x in range(0,len(self.loop)):
-            for y in range(0,len(self.loop[0])):
-                if self.loop[x][y]:
-                    self.on.blit(x*30, y*30)
-                else:
-                    self.off.blit(x*30, y*30)
-
-
-    def midi_messages(self, scale):
-
-        for i in range(0,len(self.loop[self.tt])):
-            if self.loop[self.tt][i]:
-                if not self.loop[self.tt-1][i]:
-                    self.output.send(mido.Message('note_on', note=scale[i], velocity=64))
-            else:
-                if self.loop[self.tt-1][i]:
-                    self.output.send(mido.Message('note_off', note=scale[i]))
-
-
-
-
-
+from loop import *
 
 
 # C3 = 48
@@ -117,30 +28,17 @@ a_loop = [
 
 # a_loop = loop_from_dna("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG")
 
+
+
+
+
 # create a loop object
-l = loop( mido.open_output( u'TiMidity port 0'), 
-          a_loop)
 
+l = Loop( a_loop, scale, height=200 )
 
+window = pyglet.window.Window(height=800, width=1280)
 
-def update_playhead_display(dt):
-    if l.start:
-        l.playhead_x = 1
-        l.start = False
-    else:
-        l.playhead_x += dt * 1.33 * 30
-    l.render_pianoroll()
-    l.render_playhead()
-
-def play_at_head(dt):
-    if l.tt == len(l.loop)-1:
-        l.tt = 0
-        l.start = True
-    else:
-        l.tt+=1
-    l.midi_messages(scale)
-
-pyglet.clock.schedule_interval(update_playhead_display, 1/60.0) # Update at 60Hz = 1/60
-pyglet.clock.schedule_interval(play_at_head, 60/80.0) # update at 60Hz
+pyglet.clock.schedule_interval(l.update_playhead_display, 1/60.0) # Update at 60Hz = 1/60
+pyglet.clock.schedule_interval(l.play_at_head, 60/80.0) # update at 60Hz
 pyglet.app.run()
 
