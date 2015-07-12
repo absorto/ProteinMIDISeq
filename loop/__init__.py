43d28afe8ea072f93f600f95632a00ff155ef133
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-07-11 22:38:07 -0500

    remove svg and png, red playhead, delete cruft

--- loop/__init__.py
+++ loop/__init__.py
@@ -3,7 +3,7 @@ import pprint
 
 from uuid import uuid4 as random_uuid
 import svgwrite
-from sh import convert
+from sh import convert, rm
 
 import mido
 import pyglet
@@ -27,15 +27,13 @@ class Loop:
         self.background = background
         self.bpm        = 60/bpm
         self.beat_width = float(width)/len(self.loop)
-        self.beat_height = float(height)/len(self.loop[0])
+        self.beat_height = float(height)/(len(self.loop[0]) - 1)
         self.tt         = 0
         self.start      = True
         self.vertex_index = []
         self.vertex_coords = []
         self.vertex_colors = []
         self.quads = 0
-
-        self.vertexes_from_loop()
 
         # initialize midi output
         self.midi_output = mido.open_output( midi_port )
@@ -53,13 +51,20 @@ class Loop:
         # load png
         self.sprite = pyglet.sprite.Sprite( pyglet.image.load("%s.png" % loop_hash,
                                                               file=open("%s.png" % loop_hash)))
+
+        
+        rm("%s.svg" % loop_hash)
+        rm("%s.png" % loop_hash)
+
+        
         self.sprite.x = self.x
         self.sprite.y = self.y
         
     def render_playhead(self):
         if self.playhead_x <= self.width + self.x:
             pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                                 ('v2f', (self.playhead_x, self.y, self.playhead_x, self.y+self.height)))
+                                 ('v2f', (self.playhead_x, self.y, self.playhead_x, self.y+self.height)),
+                                 ('c3B', (255,0,0, 255,0,0)))
 
 
     def render_beat(self, x, y):
@@ -68,47 +73,24 @@ class Loop:
                 x + self.beat_width, y + self.beat_height,
                 x, y+self.beat_height)
 
-
-    def vertexes_from_loop(self):
-        for x in range(0,len(self.loop)):
-            for y in range(0,len(self.loop[0])):
-                self.vertex_coords += self.render_beat(self.x + (x*self.beat_width),
-                                                         self.y + (y*self.beat_height))
-
-                self.vertex_index += [self.quads, self.quads+1, self.quads+2,
-                                      self.quads, self.quads+2, self.quads+3]
-                self.quads+=4
-
-                if self.loop[x][y]:
-                    self.vertex_colors += self.foreground * 4
-                else:
-                    self.vertex_colors += self.background * 4
-
         
     def render_pianoroll_sprite(self):
-        # here do the img, write svg on init, convert it to png, read
-        # it
         self.sprite.draw()
 
     def render_pianoroll_svg(self):
+        self.dwg.add(self.dwg.rect(insert=(0, 0),
+                                   size  =(self.width, self.height),
+                                   fill  = 'lightgoldenrodyellow', stroke_width=0))
         for x in range(0,len(self.loop)):
             for y in range(0,len(self.loop[0])):
                 if self.loop[x][y]:
                     self.dwg.add(self.dwg.rect(insert=(x*self.beat_width,
-                                                       (self.y - ((y*self.beat_height) + self.beat_height))+self.height),
+                                                       (self.height - (self.beat_height + (y*self.beat_height)))),
                                                size=(self.beat_width,
                                                      self.beat_height),
-                                               fill='midnightblue', stroke_width=0))
+                                               fill='lightsteelblue', stroke_width=0))
         self.dwg.save()
     
-
-    def render_pianoroll(self):
-        pyglet.graphics.draw_indexed(self.quads, pyglet.gl.GL_TRIANGLES,
-                                     self.vertex_index,
-                                     ('v2f', self.vertex_coords),
-                                     ('c3B', self.vertex_colors) )
-
-
 
 
     def midi_messages(self):
