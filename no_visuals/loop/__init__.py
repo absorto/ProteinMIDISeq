2630b30b3619a065fc702f0b8816a9ffd811f0df
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-03-05 16:25:18 -0600

    stop codon creates silence

--- no_visuals/loop/__init__.py
+++ no_visuals/loop/__init__.py
@@ -46,13 +46,13 @@ def loop_from_dna(sequence):
     loop = []
     # amino list from sequence
     amino_list = Seq(sequence, IUPAC.unambiguous_dna).translate()
-    
     standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
-
+    aminos = list(standard_table.back_table.keys())
+    aminos.sort()
     for amino in amino_list:
         column = [0 for n in range(0,21)]
-        if amino != '*':
-            column[list(standard_table.back_table.keys()).index(amino)] = amino
+        if not amino == None: # stop codons are None
+            column[aminos.index(amino)] = amino
 
         loop.append(column)
     return loop
