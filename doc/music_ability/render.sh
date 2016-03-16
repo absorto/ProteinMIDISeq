c976191ad37455aa33f34492369bd16f821d5428
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-03-16 00:38:16 -0500

    coding fastas rendered to midi, imported to seq24

---
+++ doc/music_ability/render.sh
@@ -0,0 +1,67 @@
+mkdir -p ACDY8
+mkdir -p AVPR1A
+mkdir -p EPHA7
+mkdir -p GALM
+mkdir -p GATA2
+mkdir -p PCDH7
+mkdir -p PCDHA1
+mkdir -p SLC6A4
+mkdir -p TRPA1
+mkdir -p UGT8.2
+mkdir -p UGT8
+mkdir -p UNC5C
+mkdir -p ZDHHC11
+
+
+cd ACDY8
+python ../../../to_midi/fasta2midi.py ../ACDY8.fasta
+cd ..
+
+cd AVPR1A
+python ../../../to_midi/fasta2midi.py ../AVPR1A.fasta
+cd ..
+
+cd EPHA7
+python ../../../to_midi/fasta2midi.py ../EPHA7.fasta
+cd ..
+
+cd GALM
+python ../../../to_midi/fasta2midi.py ../GALM.fasta
+cd ..
+
+cd GATA2
+python ../../../to_midi/fasta2midi.py ../GATA2.fasta
+cd ..
+
+cd PCDH7
+python ../../../to_midi/fasta2midi.py ../PCDH7.fasta
+cd ..
+
+cd PCDHA1
+python ../../../to_midi/fasta2midi.py ../PCDHA1.fasta
+cd ..
+
+cd SLC6A4
+python ../../../to_midi/fasta2midi.py ../SLC6A4.fasta
+cd ..
+
+cd TRPA1
+python ../../../to_midi/fasta2midi.py ../TRPA1.fasta
+cd ..
+
+cd UGT8.2
+python ../../../to_midi/fasta2midi.py ../UGT8.2.fasta
+cd ..
+
+cd UGT8
+python ../../../to_midi/fasta2midi.py ../UGT8.fasta
+cd ..
+
+cd UNC5C
+python ../../../to_midi/fasta2midi.py ../UNC5C.fasta
+cd ..
+
+cd ZDHHC11
+python ../../../to_midi/fasta2midi.py ../ZDHHC11.fasta
+cd ..
+
