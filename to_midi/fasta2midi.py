41d2715cf494e1ef27059298acefebd428dc8f18
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-03-20 17:38:06 -0500

    use transitions to set duration

--- to_midi/fasta2midi.py
+++ to_midi/fasta2midi.py
@@ -3,6 +3,8 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 from Bio.Data import CodonTable
 
+from itertools import permutations
+
 import mingus.core.scales as scales
 from mingus.containers import Note, Track, Bar
 from mingus.containers.instrument import Instrument
@@ -11,6 +13,96 @@ from mingus.midi.midi_file_out import write_Track
 import argparse
 
 from pprint import pprint
+
+ph = { 'A' : 'non polar',
+       'I' : 'non polar',
+       'F' : 'non polar',
+       'L' : 'non polar',
+       'M' : 'non polar',
+       'P' : 'non polar',
+       'V' : 'non polar',
+       'W' : 'non polar',
+       
+       'D' : 'acidic',
+       'E' : 'acidic',
+
+       'H' : 'basic',
+       'K' : 'basic',
+       'R' : 'basic',
+       
+       'G' : 'polar',
+       'C' : 'polar',
+       'N' : 'polar',
+       'Q' : 'polar',
+       'S' : 'polar',
+       'T' : 'polar',
+       'Y' : 'polar',
+       '*' : 'stop'
+}
+
+
+durations = {transition:None for transition in permutations(['polar', 'non polar', 'acidic', 'basic', 'stop'], 2)}
+
+durations = {
+    ('acidic', 'acidic'): 1,    
+    ('acidic', 'basic'): 1,
+    ('basic', 'acidic'): 1,    
+    ('stop', 'non polar'): 1,
+    ('stop', 'polar'): 1,
+    ('stop', 'stop'): 1,
+    ('stop', 'acidic'): 1,
+    ('stop', 'basic'): 1,
+    
+    ('non polar', 'polar'): 16,
+    ('polar', 'non polar'): 16,
+    ('non polar', 'non polar'): 8,
+    ('polar', 'polar'): 8,             
+
+    
+    ('acidic', 'non polar'): 8,
+    ('basic', 'non polar'): 8,
+    ('acidic', 'polar'): 4,
+    ('polar', 'acidic'): 4,    
+    ('polar', 'basic'): 4,
+    ('basic', 'basic'): 4,
+    ('basic', 'polar'): 4,
+    ('non polar', 'acidic'): 4,
+    ('non polar', 'basic'): 4,
+
+    
+    ('acidic', 'stop'): 2,
+    ('basic', 'stop'): 2,
+    ('non polar', 'stop'): 2,
+    ('polar', 'stop'): 2,
+}
+
+count = {('acidic', 'basic'): 0,
+             ('acidic', 'non polar'): 0,
+             ('acidic', 'polar'): 0,
+             ('acidic', 'stop'): 0,
+             ('basic', 'acidic'): 0,
+             ('basic', 'non polar'): 0,
+             ('basic', 'polar'): 0,
+             ('basic', 'stop'): 0,
+             ('non polar', 'acidic'): 0,
+             ('non polar', 'basic'): 0,
+             ('non polar', 'polar'): 0,
+             ('non polar', 'stop'): 0,
+             ('polar', 'acidic'): 0,
+             ('polar', 'basic'): 0,
+             ('polar', 'non polar'): 0,
+             ('polar', 'stop'): 0,
+             ('stop', 'acidic'): 0,
+             ('stop', 'basic'): 0,
+             ('stop', 'non polar'): 0,
+             ('stop', 'polar'): 0,
+             ('acidic', 'acidic'): 0,
+             ('basic', 'basic'): 0,
+             ('stop', 'stop'): 0,
+             ('non polar', 'non polar'): 0,
+             ('polar', 'polar'): 0,
+}
+
 
 def track_from_dna(sequence, scale):
 
@@ -28,54 +120,23 @@ def track_from_dna(sequence, scale):
     # translate sequence argument to amino sequence
     amino_sequence = Seq(sequence, IUPAC.unambiguous_dna).translate()
 
-    for amino_note in amino_sequence:
+    # populate track from amino-notes in the amino-sequence!
+    for i in range(len(amino_sequence)):
+        amino_note = amino_sequence[i]
+        duration = durations[(ph[amino_sequence[i-1]],ph[amino_note])]
+        count[(ph[amino_sequence[i-1]],ph[amino_note])] += 1
         if amino_note == "*":
-            pass
+            b = Bar()
+            b.place_rest(duration)
+            track.add_bar(b)
         else:
-            track.add_notes( scale[standard_aminos.index(amino_note)] )
+            track.add_notes( scale[standard_aminos.index(amino_note)], duration )
 
+    pprint(count)
     return track
 
 
 
-
-    
-
-def loop_from_dna(sequence):
-    loop = []
-    # amino list from sequence
-    amino_list = Seq(sequence, IUPAC.unambiguous_dna).translate()
-    standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
-    aminos = list(standard_table.back_table.keys())
-    aminos.remove(None)
-    aminos.sort()
-    for amino in amino_list:
-        column = [0 for n in range(0,21)]
-        if amino != "*":
-            column[aminos.index(amino)] = amino
-
-        loop.append(column)
-    return loop
-
-
-
-def midi_from_loop(loop, path):
-    scale = []
-    for octave in range(3,6):
-        for note in scales.Major("C").ascending():
-            scale.append( Note(note, octave) )
-
-    track = Track(Instrument())
-    b = Bar()
-    b.set_meter((6,8))
-    track.add_bar(b)
-
-    for t in range(0,len(loop)):
-        for g in range(len(loop[t])):
-            if loop[t][g]:
-                track.add_notes( scale[g] )
-
-    write_Track(path, track, bpm=240)
 
 
 parser = argparse.ArgumentParser(description='Convert multirecord fasta file into many MIDI files')
